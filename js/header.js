953cf5e5b1e89f9b98f9d9eb44516dc5497452d2
laidasan <tp6m4xup6tp6@gmail.com>
2020-01-18 23:47:34 +0800

    checkSuccess finished

--- js/header.js
+++ js/header.js
@@ -1,9 +1,9 @@
-(function() {
+;(function() {
     const nav = document.querySelector('.header__nav');
     const navContainer = nav.parentElement;
     const cancel = document.querySelector('.header__nav__cancel');
-    console.log(cancel)
-    console.log(navContainer)
+    // console.log(cancel)
+    // console.log(navContainer)
 
     navContainer.addEventListener('click',(e) => {
         if(e.target.className.includes('cancel')) {
